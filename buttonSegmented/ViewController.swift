e6b775d32d5fdf74d5303f77e747a6fac3cc715d
Taha Sönmez <info@tahasonmez.com>
2019-06-13 14:44:51 +0300

    Initial Commit

---
+++ buttonSegmented/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buttonSegmented
+//
+//  Created by Taha Sönmez on 13.06.2019.
+//  Copyright © 2019 Taha Sönmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
